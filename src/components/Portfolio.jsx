1465d1e7751f48e1f7bfbd8178c64ff979b366f7
Weil Essaisi <weil-97@hotmail.com>
2024-03-18 11:00:22 +0100

    init

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import spotify from "../assets/spotify-bild.jpg";
 import kanban from "../assets/kanban-bild.jpg";
-import klarna from "../assets/klarna-bild.jpg";
+import agecalculator from "../assets/agecalculator.jpg";
+
 import restaurantpage from "../assets/restaurant-bild.jpg";
 import Project from "./Project";
 
@@ -13,7 +14,7 @@ const projects = [
     description:
       "A dynamic Kanban board application designed to streamline project management and workflow visualization. This interactive tool allows users to create, move, and manage tasks with ease, promoting better organization and productivity in team settings or for personal use. Built with modern web technologies, it embodies efficient task-tracking in a sleek, user-friendly interface.",
     link: "https://incredible-gnome-bdceb0.netlify.app",
-    tech: ["React", "CSS"],
+    tech: ["Typescript", "SCSS"],
   },
   {
     id: 2,
@@ -27,12 +28,12 @@ const projects = [
   },
   {
     id: 3,
-    src: klarna,
-    title: "💳 Klarna API Project",
+    src: agecalculator,
+    title: " 📅Age calculator",
     description:
-      "This project showcases an integration with Klarna's API, providing a seamless checkout experience. Users can simulate secure payment transactions with the convenience and reliability of Klarna's services. The implementation demonstrates a solid understanding of payment gateway integrations within a simulated e-commerce environment.",
-    link: "https://klarna.herokuapp.com/",
-    tech: ["JavaScript", "CSS"],
+      " An intuitive and user-friendly tool designed to quickly and accurately calculate a user's exact age from their birth date. This calculator merges modern design with functional simplicity, making it an indispensable tool for anyone eager to know their precise age in days, months, or years.",
+    link: "https://exquisite-snickerdoodle-e258ce.netlify.app/",
+    tech: ["React", "CSS"],
   },
 
   {
@@ -43,7 +44,7 @@ const projects = [
       "A tastefully designed landing page for a modern restaurant, aiming to attract food enthusiasts and reservation bookings. The page showcases the restaurant's ambiance, menu, and specialties while providing a user-friendly platform for customers to interact with the business. It features high-quality visuals, responsive design, and an easy navigation structure for an optimal user experience.",
 
     link: "https://helpful-licorice-c6a0c5.netlify.app",
-    tech: ["React", "SCSS"],
+    tech: ["React", "CSS"],
   },
 ];
 
